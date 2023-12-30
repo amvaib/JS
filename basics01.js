55f3d3854bfd07c7959f55bf2c9d5e97c6b342e0
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-12-30 16:39:27 +0530

    basics added

---
+++ basics01.js
@@ -0,0 +1,13 @@
+const accountId = 14453;
+
+let accountEmail = "vaibhavlovescode@gmail.com";
+let accountPassword = "12345";
+
+let accountCity = "Delhi";
+
+accountEmail = "hellovaibhavgautam@gmail.com"
+console.table([accountId,accountEmail,accountCity,accountPassword]);
+
+
+
+
