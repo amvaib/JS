5cb8bfc846e115bbc533ccadb93d132728cba2ca
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-12-30 23:47:05 +0530

    typeconversion added

---
+++ typeconversion.js
@@ -0,0 +1,10 @@
+let isLoggedIn = "Vaibhav"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+
+console.log(booleanisLoggedIn)
+
+let someNumber = 234232
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
