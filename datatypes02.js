aa501da859bd75caf2b9885f687b234276a5ea5c
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-12-30 16:43:02 +0530

    02

---
+++ datatypes02.js
@@ -0,0 +1,15 @@
+let age = 32;
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof age)
+
+
